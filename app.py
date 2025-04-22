d36da1da983493c9f9cb2e82651f0ac7974c5345
ciamourwoo <ditactrao@gmail.com>
2025-04-23 00:13:02 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Konfigurasi halaman
+st.set_page_config(page_title="PathMath", layout="centered")
+
+# Isi halaman aplikasi
+st.title("Selamat Datang di PathMath - Sistem Rekomendasi Soal Matematika")
+st.write("Ayo mulai perjalananmu dalam memahami matematika dengan soal yang tepat!")
