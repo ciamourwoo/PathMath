4aee324f3e7bfcfe110901bc4a2f9dfd6df3ebb5
ciamourwoo <ditactrao@gmail.com>
2025-04-24 09:46:22 +0700

    Update app.py

--- Downloads/pathmath/app.py
+++ Downloads/pathmath/app.py
@@ -10,15 +10,13 @@ st.write("Ayo mulai perjalananmu dalam memahami matematika dengan soal yang tepa
 # Form input identitas siswa
 with st.form("form_identitas"):
     nama = st.text_input("Nama Lengkap")
-    kelas = st.selectbox("Kelas", ["4", "5", "6"])
     materi = st.selectbox("Materi yang akan dikerjakan", ["Pecahan", "Pola Bilangan", "KPK dan FPB", "Luas dan Volume", "Bangun Datar"])
     
     submit_button = st.form_submit_button("Mulai Mengerjakan")
     
 # Jika tombol diklik
 if submit_button:
-    st.success(f"Halo {nama} dari kelas {kelas}, selamat mengerjakan materi {materi}!")
+    st.success(f"Halo {nama} , selamat mengerjakan materi {materi}!")
     # Simpan data identitas siswa ke session (opsional)
     st.session_state["nama"] = nama
-    st.session_state["kelas"] = kelas
     st.session_state["materi"] = materi
